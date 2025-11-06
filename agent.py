99274063e42d7f5efcfb55dd4544cca5edb738d2
Shashwat Shukla <shashwats018@gmail.com>
2025-11-07 03:02:29 +0530

    tellu msg receives and processed

--- agent.py
+++ agent.py
@@ -1,10 +1,10 @@
-from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_google_genai import GoogleGenerativeAI
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-llm = ChatGoogleGenerativeAI(model="gemini-2.5-pro")
+llm = GoogleGenerativeAI(model="gemini-2.5-pro")
 response = llm.invoke("Sing a ballad of LangChain.")
 
-print(response.content)
+print(response )
